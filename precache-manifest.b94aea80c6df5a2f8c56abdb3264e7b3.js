04f2b3e1c1c57f6cac17b9d04d8b4aae737d54fb
rshn-doomhammer <roshanpatel06@gmail.com>
2020-05-26 22:22:51 -0400

    Updates

--- precache-manifest.b94aea80c6df5a2f8c56abdb3264e7b3.js
+++ precache-manifest.b94aea80c6df5a2f8c56abdb3264e7b3.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "000a70e0f2aa5f68054c2f83fc1d6605",
+    "revision": "2178431db6a78cd79a9e27b33b227565",
     "url": "/index.html"
   },
   {
-    "revision": "d8e676ca65d9aab5c7ef",
-    "url": "/static/css/main.50fd53a7.chunk.css"
+    "revision": "acd142b7ffd38abc4ac1",
+    "url": "/static/css/main.1ff08014.chunk.css"
   },
   {
     "revision": "3e416789baba60e007de",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.a617a02f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d8e676ca65d9aab5c7ef",
-    "url": "/static/js/main.65d76a92.chunk.js"
+    "revision": "acd142b7ffd38abc4ac1",
+    "url": "/static/js/main.2a297a7a.chunk.js"
   },
   {
     "revision": "b4ba650b238755144e2a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7f4748a4aa063cab7b5c5dcbb68610a2.js"
+  "/precache-manifest.b94aea80c6df5a2f8c56abdb3264e7b3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
