b8a6ef54634d788ff7b728202d80b2dc0029d9f3
rshn-doomhammer <roshanpatel06@gmail.com>
2020-07-15 14:41:09 -0400

    updates to poke

---
+++ build/precache-manifest.604829df6177a97af6916bb1e9ca66d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c169260b479aba5f3e046a4ff95d1fb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71a6ffe0f9cad56673fc",
+    "url": "/static/css/main.ea6990af.chunk.css"
+  },
+  {
+    "revision": "6bb64744ce24d0b410f1",
+    "url": "/static/js/2.18e4ac39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.18e4ac39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71a6ffe0f9cad56673fc",
+    "url": "/static/js/main.7f888df3.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  }
+]);
