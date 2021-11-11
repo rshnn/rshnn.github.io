da3d6f29253577234c9083fec5c3bb388ba9a5de
rshn-doomhammer <roshanpatel06@gmail.com>
2021-11-11 09:54:46 -0500

    adds images from build

---
+++ build/precache-manifest.22a1bacce5cfc3bc206c53c6b0b9cb7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9daddb7e1517e0353d88f60aed285a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d26bae677a895e092217",
+    "url": "/static/css/main.cd5e2f6e.chunk.css"
+  },
+  {
+    "revision": "314a97daf3210d0f72f7",
+    "url": "/static/js/2.a46f626e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.a46f626e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d26bae677a895e092217",
+    "url": "/static/js/main.f009b01c.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  }
+]);
